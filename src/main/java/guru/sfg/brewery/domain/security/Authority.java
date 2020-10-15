60fc51a180015c98fe624000d14b26f12d8bd498
sergiishamrai-okta <71881713+sergiishamrai-okta@users.noreply.github.com>
2020-10-15 16:14:13 +0300

    JPA User, JPA Authority

---
+++ src/main/java/guru/sfg/brewery/domain/security/Authority.java
@@ -0,0 +1,24 @@
+package guru.sfg.brewery.domain.security;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String role;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
+}
